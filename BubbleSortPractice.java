b8d947bb1d93992bb51f579eed5093d975473963
Abraham Vallejos-Soto <125098951+AVSbeats@users.noreply.github.com>
2023-03-09 09:30:19 -0500

    Changes

--- BubbleSortPractice.java
+++ BubbleSortPractice.java
@@ -1,19 +1,33 @@
 public class BubbleSortPractice {
     public static void main(String[] args) {
 
-        int[] someNumbers = { 20, 5, 15, 10 };
+        String[] names = { "Irma", "Abraham", "Kevin", "Yvonne", "Maria" };
 
-        int temp;
+        String temp;
+        int a;
         int b;
 
-        for (b = 0; b < someNumbers.length - 1; b++) {
-            if (someNumbers[b] > someNumbers[b + 1]) {
-                temp = someNumbers[b];
-                someNumbers[b] = someNumbers[b + 1];
-                someNumbers[b + 1] = temp;
+        int comparision = names.length - 1;
+
+        // This will move the orders from Greater to lowest
+        for (a = 0; a < comparision; ++a) {
+            for (b = 0; b < comparision; ++b) {
+                if (names[b].compareTo(names[b + 1]) > 0) {
+
+                    temp = names[b];
+                    names[b] = names[b + 1];
+                    names[b + 1] = temp;
+
+                }
 
             }
+
         }
-        System.out.println(someNumbers);
+        --comparision;
+
+        // This is to display the final String outcome
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
